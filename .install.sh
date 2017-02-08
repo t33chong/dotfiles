5f54123d0449a7d2b1984ab55b0ba99da21e4c16
Tristan Chong <tristanchong@gmail.com>
2017-02-08 15:14:46 -0800

    Idiot-proof against destructive symlinking

--- .install.sh
+++ .install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $PWD == $HOME ]; then
+  echo "Run this from your dotfiles repo, not your home directory!"
+  exit 0
+fi
+
 ln -sF $PWD/.* $HOME
 unlink $HOME/.git
 unlink $HOME/.gitignore
